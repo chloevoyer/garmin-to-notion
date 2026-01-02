525809d747c019067b08891cf8f9e0ef681bd3c2
Garibel <Garibel.Lee@gmail.com>
2026-01-03 00:51:17 +0800

    Update sleep-data.py

--- sleep-data.py
+++ sleep-data.py
@@ -89,7 +89,7 @@ def main():
     database_id = os.getenv("NOTION_SLEEP_DB_ID")
 
     # Initialize Garmin client and login
-    garmin = Garmin(garmin_email, garmin_password)
+    garmin = Garmin(garmin_email, garmin_password, is_cn=True)
     garmin.login()
     client = Client(auth=notion_token)
 
